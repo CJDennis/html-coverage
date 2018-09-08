ca4723cc3acbfc67b37682f474186747ddefb8d2
Daniel Klein <danielklein@airpost.net>
2018-09-08 21:23:24 +1000

    Commit initial code and tests

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php echo 1; ?>
+<?php echo 2;
+?>
+<?php
+echo 3; ?>
+<?php
+echo 4;
+?>
+<?php echo 5; ?>
+<?php echo 6;
+?>
+<?php
+echo 7; ?>
+<?php
+echo 8;
+?>
---
+++ tests/unit/CoverageTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class CoverageTest extends \Codeception\Test\Unit {
+  /**
+   * @var \UnitTester
+   */
+  protected $tester;
+
+  protected function _before() {
+  }
+
+  protected function _after() {
+  }
+
+  // tests
+  public function testSomeFeature() {
+    include 'index.php';
+    $this->assertTrue(true);
+  }
+}
